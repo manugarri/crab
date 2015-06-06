6357814f7492783c37554204a9696df40c08163c
Manuel Garrido <manuel.garrido.pena@gmail.com>
2015-06-05 22:46:24 -0400

    Update __init__.py

--- lib/crab/__init__.py
+++ lib/crab/__init__.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
- __author__ = 'manugarri'
+__author__ = 'manugarri'
 class CrabError(Exception):
     """Base class for exceptions raised internally by crab.
 
